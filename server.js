1889f0c0929a444207d06d2d7eff4032799c649c
Brianna Workman <brianna.workman@hotmail.com>
2019-12-09 18:18:20 -0600

    MVP complete

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./accounts/accounts-router");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", db);
+
+server.get("/", (req, res) => {
+  res.send("** Server running on port 4000 **");
+});
 
 module.exports = server;
